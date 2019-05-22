7a9f0c0c5dd4c37fefbf15e567481b6490ee9e67
nxjm <aut456@163.com>
2019-05-22 22:43:59 +0800

    change main.cpp

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	string str;
+	while(getline(cin, str)){
+		if (str.empty())break;
+		cout << "me:" << str << endl;
+	}
 	cout << "I Love you,My wife!" << endl;
 	return 0;
 }
